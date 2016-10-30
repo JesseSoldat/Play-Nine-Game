7962e266cff5eb66a9f12f05516995fde3263a92
jesse soldat <jsoldat@hotmail.com>
2016-10-30 14:11:00 +0700

    basic nav layout

---
+++ src/app/components/play-nine/Game.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Game extends Component {
+
+	render(){
+		return(
+		<div>
+			<h2>Play Nine</h2>
+		</div>
+		);
+	}
+}
+
+export default Game;
--- src/app/routes.js
+++ src/app/routes.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
 import App from './components/App';
+import Game from './components/play-nine/Game';
 
 export default (
 <Route path="/" component={App}>
+	<Route path="/play-nine" component={Game}/>
 </Route>
 )
